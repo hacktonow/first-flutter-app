739e675bc994b4055a33e46c46efd521c2aa22e1
hacktonow <hacktonow@gmail.com>
2021-03-31 06:57:18 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,33 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Best App"),
-      ),
-      body: Container(
-          child: Center(
-        child: Text("Hi Majhool"),
-      )),
-    );
+        appBar: AppBar(
+          title: Text("Best App"),
+        ),
+        body: Center(
+            child: Container(
+                padding: EdgeInsets.all(8),
+                alignment: Alignment.center,
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.teal,
+                  gradient: LinearGradient(colors: [
+                    Colors.red,
+                    Colors.yellow,
+                  ]),
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[400],
+                        blurRadius: 5,
+                        offset: Offset(2.0, .0))
+                  ],
+                ),
+                child: Text("I Am A Box",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    )))));
   }
 }
