a0ac13322a7e34f8238d9ebdf268160a392a679e
hacktonow <hacktonow@gmail.com>
2021-03-31 12:03:01 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'dart:ui';
 
 void main() {
   runApp(MaterialApp(
     title: "Best App",
     home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
   ));
 }
 
@@ -12,45 +16,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Best App"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              color: Colors.black,
-              width: 200,
-              height: 400,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.green,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.yellow,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
-              ),
-            ),
-          ),
+      body: Center(
+        child: Column(
+          children: <Widget>[Image.asset("assets/sb.jpg")],
         ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            //DrawerHeader(
+            //child: Text(
+            //"hi I Am Drawer",
+            //style: TextStyle(color: Colors.white),
+            //),
+            //decoration: BoxDecoration(color: Colors.indigo),
+            //),
+            UserAccountsDrawerHeader(
+              accountName: Text("Majhool"),
+              accountEmail: Text("hacktonow@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1484186304838-0bf1a8cff81c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80")),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
       ),
     );
   }
